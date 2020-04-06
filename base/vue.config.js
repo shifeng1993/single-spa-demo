d4379efdaa040133a1a11026c560b21d7c37c683
shifeng <shifeng199307@gmail.com>
2020-04-06 13:12:58 +0800

    修复bug

--- base/vue.config.js
+++ base/vue.config.js
@@ -31,6 +31,9 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   outputDir: path.join(__dirname, '../dist/'),
   assetsDir: 'baseModule/static',
+  // chainWebpack: config => {
+  //   config.externals(['vue', 'vue-router']);
+  // },
   configureWebpack: {
     entry: {
       app: './src/main.js',
@@ -41,37 +44,7 @@ module.exports = {
       chunkFilename: 'baseModule/static/js/[name].[hash].js' // commonChunk 输出文件
     },
     plugins: [
-    ],
-    optimization: {
-      splitChunks: {
-        chunks: 'async',  // 默认只分割异步代码块 import() 'async' 'initial' 'all'
-        // minSize: 30000,   // 生成块的最小 单位字节
-        // maxSize: 0,       // 生成块的最大 单位字节
-        // minChunks: 1,     // 分割前必须共享模块的最小块数。
-        // maxAsyncRequests: 5,  // 异步代码块最大并发请求数量
-        // maxInitialRequests: 3,  // 入口处的最大并发请求数量
-        // automaticNameDelimiter: '~', // 生成名称时所用的分隔符
-        // name: true,
-        cacheGroups: {
-          vendors: {
-            test: /[\\/]node_modules[\\/]/,
-            priority: -10  // 优先级
-          },
-          // default是打包出来的名称，可以修改，例如改成common
-          default: {
-            minChunks: 2,
-            minSize: 0, // 上面的minsize是默认的配置，这个minsize是自己配置的，所以优先级高
-            priority: -20, // 优先级
-            reuseExistingChunk: true
-          },
-          // 第三方库分离示例 匹配react 和 react-dom，完成打包的分离
-          store: {
-            test: /[\\/]src[\\/]store/,
-            priority: -10  // 优先级
-          },
-        }
-      }
-    }
+    ]
   },
   productionSourceMap: false, // 生产环境的sourcemap
   devServer: {
